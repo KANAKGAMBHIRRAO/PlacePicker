d1ba6b834250e00ea26d12216d6852e376b27145
kanakg <kanakgambhirrao123@gmail.com>
2024-05-18 14:17:46 +0530

    full project

---
+++ src/components/AvailablePlaces.jsx
@@ -0,0 +1,80 @@
+import { useState, useEffect } from "react";
+
+import Places from "./Places.jsx";
+import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
+import { useFetch } from "../../hook/useFetch.js";
+
+async function fetchSortedPlaces() {
+  const places = await fetchAvailablePlaces();
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      resolve(sortedPlaces);
+      // setAvailablePlaces(sortedPlaces);
+      // setIsFetching(false);
+    });
+    // resolve(sortedPlaces);
+  });
+}
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  // const [isFetching, setIsFetching] = useState(false);
+  // const [availablePlaces, setAvailablePlaces] = useState([]);
+  // const [error, setError] = useState();
+
+  const {
+    isFetching,
+    error,
+    fetchedData: availablePlaces,
+    // setFetchedData: setAvailablePlaces,
+  } = useFetch(fetchSortedPlaces, []);
+
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />;
+  }
+
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
+
+// useEffect(() => {
+//   async function fetchPlaces() {
+//     setIsFetching(true);
+
+//     try {
+//       const places = await fetchAvailablePlaces();
+
+//       navigator.geolocation.getCurrentPosition((position) => {
+//         const sortedPlaces = sortPlacesByDistance(
+//           places,
+//           position.coords.latitude,
+//           position.coords.longitude
+//         );
+//         setAvailablePlaces(sortedPlaces);
+//         setIsFetching(false);
+//       });
+//     } catch (error) {
+//       setError({
+//         message:
+//           error.message || 'Could not fetch places, please try again later.',
+//       });
+//       setIsFetching(false);
+//     }
+//   }
+
+//   fetchPlaces();
+// }, []);
